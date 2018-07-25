365148310329541024b6f50bc5b4b53b91540a97
Warrick FitzGerald <warrick@eosnewyork.io>
2018-07-25 09:57:37 -0400

    Code cleanup MD5 Hasha and change of folder structure

--- EOSAccountAnalyzer/Program.cs
+++ EOSAccountAnalyzer/Program.cs
@@ -16,6 +16,8 @@ using System.Threading.Tasks;
 using Amazon.Extensions.NETCore.Setup;
 using Amazon.Runtime.CredentialManagement;
 using Amazon.Runtime;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace EOSAccountAnalyzer
 {
@@ -25,7 +27,6 @@ namespace EOSAccountAnalyzer
         {
             Logger logger = NLog.LogManager.GetCurrentClassLogger();
             Args.InvokeAction<GetProgram>(args);
-            Console.ReadLine();
         }
     }
 
@@ -39,78 +40,78 @@ namespace EOSAccountAnalyzer
         public bool Help { get; set; }
 
         [ArgActionMethod, ArgDescription("Retrieve data from one of the well known EOS tables")]
-        public void expand([ArgRequired]expandArguments arg)
+        public void Expand([ArgRequired]ExpandArguments arg)
         {
 
-            logger.Info("Expanding contacts contained in file \"{0}\".", arg.filePath);
-            if (File.Exists(arg.filePath))
+            logger.Info("Expanding contacts contained in file \"{0}\".", arg.FilePath);
+            if (File.Exists(arg.FilePath))
             {
-                FileInfo accountsFileInfo = new FileInfo(arg.filePath);
-                EOSInfoCollector collector = new EOSInfoCollector(accountsFileInfo);
-                if(arg.skipDownload)
+                FileInfo accountsFileInfo = new FileInfo(arg.FilePath);
+                EOSInfoCollector collector = new EOSInfoCollector(accountsFileInfo, arg.OutputPath);
+                if(arg.SkipDownload)
                 {
-                    logger.Warn("Skipping the download of data and will instead process the json files that exist in {0}", arg.outputPath);
+                    logger.Warn("Skipping the download of data and will instead process the json files that exist in {0}", arg.OutputPath);
                 }
                 else
                 {
-                    var fetchfiles = collector.startAsync(arg.outputPath, arg.overwrite, arg.apihost).Result;
+                    var fetchfiles = collector.StartAsync(arg.Overwrite, arg.Apihost).Result;
                 }               
                 Console.WriteLine();
-                if (collector.validateFileCount(arg.outputPath))
+                if (collector.ValidateFileCount())
                 {
-                    logger.Info("Calculate the balance for each account");
-                    collector.calcTotalBalance(arg.outputPath);
-                    collector.compressOutput(arg.outputPath,arg.zipoutput);
-                    collector.uploadToS3(arg.zipoutput,arg.s3bucket, arg.s3profile);
+                    logger.Info("Calculting the balance for each account");
+                    collector.CalcTotalBalance();
+                    collector.GenerateMD5();
+                    collector.CompressOutput(arg.Zipoutput);
+                    collector.UploadToS3(arg.Zipoutput,arg.S3bucket, arg.S3profile);
                     logger.Info("Done");
 
                 } else
                 {
-                    logger.Error("The number of files in the staging are do not match the number of accounts provided", arg.filePath);
+                    logger.Error("The number of files in the staging are do not match the number of accounts provided", arg.FilePath);
                 }
             }
             else
             {
-                logger.Error("File \"{0}\" does not exist or is inaccessible", arg.filePath);
+                logger.Error("File \"{0}\" does not exist or is inaccessible", arg.FilePath);
                 Environment.Exit(-1);
             }
         }
     }
 
-    public class expandArguments
+    public class ExpandArguments
     {
         [ArgDefaultValue("contacts.txt")]
         [ArgDescription("The path to the file containing the flat list of contacts you'd like to expand"), ArgPosition(1)]
-        public String filePath { get; set; }
+        public String FilePath { get; set; }
 
         [ArgDefaultValue("output")]
         [ArgDescription("The path to the directory you'd like to output to"), ArgPosition(2)]
-        public String outputPath { get; set; }
+        public String OutputPath { get; set; }
 
         [ArgDefaultValue(false)]
         [ArgDescription("If the output directory path exists, delete and create start with an empty directory."), ArgPosition(3)]
-        public bool overwrite { get; set; }
+        public bool Overwrite { get; set; }
 
         [ArgDefaultValue("http://pennstation.eosdocs.io:7001")]
         [ArgDescription("The url of the EOS API"), ArgPosition(4)]
-        public Uri apihost { get; set; }
+        public Uri Apihost { get; set; }
 
         [ArgDefaultValue(false)]
         [ArgDescription("Process the files in the existing output directory. Do not re-download the data."), ArgPosition(5)]
-        public bool skipDownload { get; set; }
+        public bool SkipDownload { get; set; }
 
         [ArgDefaultValue("summary.zip")]
         [ArgDescription("The path to the ZIP file that will contain the raw data and summary file."), ArgPosition(6)]
-        public String zipoutput { get; set; }
+        public String Zipoutput { get; set; }
 
         [ArgDefaultValue("eossnapshots-staticsitebucket-zp2kemxur4pw")]
         [ArgDescription("The name of the S3 bucket that the compressed file will be uploaded to."), ArgPosition(7)]
-        public String s3bucket { get; set; }
+        public String S3bucket { get; set; }
 
         [ArgDefaultValue("publicwebsitefileupload")]
         [ArgDescription("The name of the S3 profile which contains the required credentials for upload."), ArgPosition(8)]
-        public String s3profile { get; set; }
-
+        public String S3profile { get; set; }
 
     }
 
@@ -119,9 +120,15 @@ namespace EOSAccountAnalyzer
     {
         Logger logger = NLog.LogManager.GetCurrentClassLogger();
         List<string> contactList = new List<string>();
+        String OutputDirectory { get; set; }
+        String DownloadDirectory { get; set; }
+        String BalanceFile { get; set; }
 
-        public EOSInfoCollector(FileInfo file)
+        public EOSInfoCollector(FileInfo file, String outputPath)
         {
+            OutputDirectory = outputPath;
+            DownloadDirectory = Path.Combine(outputPath, "rawdata");
+
             logger.Info("Loding all contacts contained in \"{0}\" into memory.", file.FullName);
             var logFile = File.ReadAllLines(file.FullName);
             contactList = new List<string>(logFile);
@@ -129,14 +136,14 @@ namespace EOSAccountAnalyzer
 
         }
 
-        public async Task<bool> startAsync(String outputDirectory, bool overwrite, Uri apihost)
+        public async Task<bool> StartAsync(bool overwrite, Uri apihost)
         {
-            if (Directory.Exists(outputDirectory))
+            if (Directory.Exists(OutputDirectory))
             {
                 if (overwrite)
                 {
-                    logger.Warn("overwrite = true, Deleting existing output directory {0}", outputDirectory);
-                    Directory.Delete(outputDirectory, true);                    
+                    logger.Warn("overwrite = true, Deleting existing output directory {0}", OutputDirectory);
+                    Directory.Delete(OutputDirectory, true);                    
                 } else
                 {
                     //throw new Exception("Directory exists, but overwrite option set to fale.");
@@ -145,48 +152,43 @@ namespace EOSAccountAnalyzer
                 }
             }
 
-            logger.Info("Creating output directory {0}", outputDirectory);
-            Directory.CreateDirectory(outputDirectory);
+            logger.Info("Creating output directory \"{0}\"", OutputDirectory);
+            Directory.CreateDirectory(OutputDirectory);
+            Directory.CreateDirectory(DownloadDirectory);
 
-            Task[] requests = contactList.Select(l => new EOS_Object<EOSAccount_row>(apihost).getAllObjectRecordsAsync(new EOSAccount_row.postData() { account_name = l }))
-                        .Select(r => HandleResponse(r, outputDirectory))
+            logger.Info("Starting Async download of raw contact information", OutputDirectory);
+            Task[] requests = contactList.Select(accountName => new EOS_Object<EOSAccount_row>(apihost).getAllObjectRecordsAsync(new EOSAccount_row.postData() { account_name = accountName }))
+                        .Select(r => HandleResponse(r))
                         .ToArray();
-
             await Task.WhenAll(requests);
             return true;
         }
 
-        private async Task HandleResponse(Task<EOSAccount_row> accountTask, string outputdir)
+        private async Task HandleResponse(Task<EOSAccount_row> accountTask)
         {
             Console.Write('.');
             var account = accountTask.Result;
             string json = JsonConvert.SerializeObject(account, Formatting.Indented);
-            await File.WriteAllTextAsync(Path.Combine(outputdir, account.account_name + ".txt"), json);
+            await File.WriteAllTextAsync(Path.Combine(DownloadDirectory, account.account_name + ".txt"), json);
         }
 
-        public bool validateFileCount(String outputDirectory)
+        public bool ValidateFileCount()
         {
             bool match = false;
-            var fileCount = Directory.GetFiles(outputDirectory).Length;
+            var fileCount = Directory.GetFiles(DownloadDirectory).Length;
             if (fileCount == contactList.Count)
                 match = true;
 
             return match;
         }
 
-        public void calcTotalBalance(String outputDirectory)
+        public void CalcTotalBalance()
         {
-            var summaryDirectory = Path.Combine(outputDirectory, "summary");
-            if (Directory.Exists(summaryDirectory))
-                Directory.Delete(summaryDirectory, true);
+            BalanceFile = Path.Combine(OutputDirectory, "balances.csv");
 
-            logger.Info("Creating summary directory {0}", summaryDirectory);
-            Directory.CreateDirectory(summaryDirectory);
-            var balanceFile = Path.Combine(summaryDirectory, "balances.csv");
-
-            using (StreamWriter sw = File.CreateText(balanceFile))
+            using (StreamWriter sw = File.CreateText(BalanceFile))
             {
-                foreach (var file in Directory.GetFiles(outputDirectory))
+                foreach (var file in Directory.GetFiles(DownloadDirectory))
                 {
                     var account = JsonConvert.DeserializeObject<EOSAccount_row>(File.ReadAllText(file));
                     sw.WriteLine(string.Format("{0},{1},{2},{3}",account.account_name, account.self_delegated_bandwidth.cpu_weight, account.self_delegated_bandwidth.net_weight,account.core_liquid_balance_decimal));
@@ -194,29 +196,43 @@ namespace EOSAccountAnalyzer
             }
         }
 
-
-        public void compressOutput(String outputDirectory, string zipOutputPath)
+        public void GenerateMD5()
         {
-            logger.Info("Compress contents of {0} -> {1}", outputDirectory, zipOutputPath);
-            if (File.Exists(zipOutputPath))
-                File.Delete(zipOutputPath);
-            ZipFile.CreateFromDirectory(outputDirectory, zipOutputPath);
+            logger.Info("Calculate MD5 of {0}", BalanceFile);
+            string md5HashString = string.Empty;
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(BalanceFile))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    md5HashString = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+            logger.Info("MD5Hash = {0}", md5HashString);
+            File.WriteAllText(Path.Combine(OutputDirectory, "MD5Hash.txt"), md5HashString);
         }
 
-        public void uploadToS3(String zipOutputPath, string s3Bucket, string s3Profile)
+        public void CompressOutput(string zipOutputPath)
+        {
+            logger.Info("Compress contents of {0} -> {1}", OutputDirectory, zipOutputPath);
+            if (File.Exists(zipOutputPath))
+                File.Delete(zipOutputPath);
+            ZipFile.CreateFromDirectory(OutputDirectory, zipOutputPath);
+        }
+
+        public void UploadToS3(String zipOutputPath, string s3Bucket, string s3Profile)
         {
             var dt = DateTime.Now;
             var bucket = s3Bucket + "/data/"+dt.ToString("yyyy-MM");
             var key = dt.ToString("yyyy-MM-dd") + ".zip";
 
-            logger.Info("Upload {0} to S3 as {1}/{2}",zipOutputPath, bucket, key);
+            logger.Info("Uploading {0} to s3://{1}/{2}",zipOutputPath, bucket, key);
             var chain = new CredentialProfileStoreChain();
-            AWSCredentials awsCredentials;
-            if (chain.TryGetAWSCredentials(s3Profile, out awsCredentials))
+            if (chain.TryGetAWSCredentials(s3Profile, out AWSCredentials awsCredentials))
             {
                 AmazonS3Client _s3Client = new AmazonS3Client(awsCredentials, RegionEndpoint.USEast1);
                 TransferUtility fileTransferUtility = new TransferUtility(_s3Client);
-                fileTransferUtility.Upload(zipOutputPath, bucket, key);
+                fileTransferUtility.Upload(zipOutputPath, bucket, key );
             }
 
         }
